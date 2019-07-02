5f106ad6ca0f8b28e7bb5a182bbefebb29a25979
Zainab Alasadi <zainab.alasadi@hotmail.com>
2019-07-02 16:59:51 +1000

    calculated accuracy

--- train.py
+++ train.py
@@ -1,26 +1,77 @@
 import numpy as np
-import sklearn
-import sklearn.datasets as ds
-import sklearn.model_selection as ms
+import cv2
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 import os 
+from glob import glob
 
-# read the data set
-folder = "data"
-dataset = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-print("Working with {0} images".format(len(dataset)))
+def processImg():
+   # create a label dict to map strings to numerals
+   label_dic = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, 
+             "six": 6, "seven": 7, "eight": 8, "nine": 9}
+
+   feat = []
+   labels = []
+
+   # find all jpg in data folder
+   folder = glob("data/*.jpg")
+   
+   # loop through each file and retrieve values + label
+   for i, file in enumerate(folder):
+      new_label = os.path.split(file)[1].split("_")[0] 
+      labels.append(label_dic[new_label])
+
+      image = cv2.imread(file)
+      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      gray = gray.reshape(400)
+      feat.append(gray)
+
+   # format as array and return
+   feat = np.asarray(feat).astype(np.float32)
+   labels = np.asarray(labels).astype(np.float32)
+
+   return feat, labels
+
+# generate features and labels
+features, labels = processImg()
+
+# split the training and testing data
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size = 0.20)
+# perform knn 
+knn = KNeighborsClassifier(n_neighbors = 5)
+# fit the knn using training data
+knn.fit(X_train, y_train)
+# predict on new dataset
+y_pred = knn.predict(X_test)
+# print accuracy score
+print(accuracy_score(y_test, y_pred))
+
+# create a confusion matrix
+con_matrix = confusion_matrix(y_test, y_pred)
+print(con_matrix)
 
 
-# spit the data set
-X_train, X_test, y_train, y_test = train_test_split(dataset, labels, test_size=0.20, random_state=42)
+# print recall and precision
+for i in range(0, 10):
+   # recall = TP / (TP + FN)
+   tp = con_matrix[i][i]
+   fn_tp = con_matrix[i, :].sum()
+   recall = tp / fn_tp
 
-# # instantiate learning model (k = 5)
-# knn = KNeighborsClassifier(n_neighbors = 5)
+   # precision = TP / (TP + FP)
+   tp = con_matrix[i][i]
+   fp_tp = con_matrix[:, 1].sum()
+   precision = tp / fp_tp
 
-# # fitting the model
-# knn.fit(X_train, y_train)
+   print("-------Class", i, "Recall-------")
+   print("True Positives:                                 ", tp)
+   print("True Positives & False Negatives:               ", fn_tp)
+   print("Recall:                                          {:.3f}%".format(recall*100), end="\n")
 
-# # evaluate the score of the trained classifier on the test dataset
-# knc.score(X_test, y_test)
+   print("-------Class", i, "Precision-------")
+   print("True Positives:                                 ", tp)
+   print("True Positives & False Negatives:               ", fp_tp)
+   print("Precision:                                       {:.3f}%".format(precision*100), end="\n\n")
